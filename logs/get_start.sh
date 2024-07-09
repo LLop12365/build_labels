ddaf4b6310256e47cfdb3a7361bfcc69ef932f9d
LLop12365 <55352061+LLop12365@users.noreply.github.com>
2024-07-09 17:18:11 +0800

    Create get_start.sh

---
+++ logs/get_start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+LOCK_FILE="/home/program/build_labels/logs/cur.lock"
+if [ -f "$LOCK_FILE" ]; then
+    PID=$(cat "$LOCK_FILE")
+    if ps -p $PID > /dev/null; then
+        echo "程序正在运行中 PID $PID. 正在等待..."
+        while ps -p $PID > /dev/null; do
+            sleep 3
+        done
+        echo "程序 PID $PID 运行完成. Proceeding..."
+    else
+        echo "Lock file存在，但是程序没有运行，移除lock file并开始执行文件."
+        rm "$LOCK_FILE"
+    fi
+fi
+
+echo $$ > "$LOCK_FILE"
+printf "\n\n"
+source /etc/profile
+date
+
+source /root/anaconda3/bin/activate labels
+python /home/program/build_labels/main.py
+rm "$LOCK_FILE"
